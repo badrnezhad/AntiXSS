feb6690f741bc9937151fad91923be01f0b1859d
hossein badrnezhad <badrnezhad@users.noreply.github.com>
2018-02-12 09:00:19 +0330

    Create AntiXSS.cs

---
+++ C/AntiXSS.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace PC.Framework
+{
+  public static class AntiXSS
+  {
+     #region *** Antix XSS ***
+
+        public static string AntiXSS(string input, bool disableHtmlEncoding = false, string allowRegex = "^[a-zA-Z0-9]*")
+        {
+            try
+            {
+                if (!Regex.IsMatch(input, allowRegex))
+                    return "Dangerous Input";
+                input = disableHtmlEncoding ? input : HttpUtility.HtmlEncode(input);
+                return input;
+            }
+            catch (Exception ex)
+            {
+                return "Error";
+            }
+        }
+
+        #endregion
+  }
+}
